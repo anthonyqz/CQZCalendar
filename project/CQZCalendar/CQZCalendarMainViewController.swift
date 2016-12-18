e9f0141521c62ca68d260ee2ebc971afce345090
Christian Quicano <anthony.qz@ecorenetworks.com>
2016-12-18 11:41:11 -0500

    commit

--- project/CQZCalendar/CQZCalendarMainViewController.swift
+++ project/CQZCalendar/CQZCalendarMainViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import FSCalendar
 
 public class CQZCalendarMainViewController: UIViewController {
 
+    //MARK: - private properties
+    @IBOutlet private weak var calendar: FSCalendar!
+    @IBOutlet private weak var calendarHeightConstraint: NSLayoutConstraint!
+    
+    //MARK: - init
     convenience init(){
         self.init(nibName: "CQZCalendarMainViewController", bundle: nil)
+        
     }
     
+    //MARK: - override
     override public func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func changeMode(sender: AnyObject) {
+//        calendar.scope = .Week
+    }
+    
+    //MARK: - Privates methods
+}
 
+//MARK: - FSCalendarDelegate, FSCalendarDataSource
+extension CQZCalendarMainViewController:FSCalendarDelegate, FSCalendarDataSource {
+    
+    //for autolayout
+    public func calendar(calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = CGRectGetHeight(bounds)
+        view.layoutIfNeeded()
+    }
+    
 }
